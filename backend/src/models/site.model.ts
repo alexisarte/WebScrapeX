6f0acd3ea9d327941cdc362dce54dcaab1db420e
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-09-25 18:38:34 -0300

    Add the entity site

---
+++ backend/src/models/site.model.ts
@@ -0,0 +1,51 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Site extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  url: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  maxDepth: number;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  frequency: number;
+
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Site>) {
+    super(data);
+  }
+}
+
+export interface SiteRelations {
+  // describe navigational properties here
+}
+
+export type SiteWithRelations = Site & SiteRelations;
