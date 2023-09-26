d6d109783c87d4258bbf6090960c2f8f4bd9d8f3
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-09-26 16:39:44 -0300

    Create database

---
+++ backend/src/datasources/mongo.datasource.ts
@@ -0,0 +1,32 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'mongo',
+  connector: 'mongodb',
+  port: 27017,
+  url: 'mongodb+srv://alexis:6ZnQthcFhlFwQvOA@cluster0.2qru803.mongodb.net/mongo?retryWrites=true&w=majority',
+  // host: '',
+  // user: '',
+  // password: 'm001-student',
+  database: 'mongo',
+  useNewUrlParser: true
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongoDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongo';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongo', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
