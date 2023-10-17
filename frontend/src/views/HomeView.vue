8d6384da9d9bcb0ae8440376193692afdba13631
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-10-16 21:40:56 -0300

    Add navigation bar

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    asdfkjajdsfalñkdskfjk
   </main>
 </template>
+<style scoped>
+</style>
+
