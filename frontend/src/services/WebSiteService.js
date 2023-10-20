ff27d4e722de9a26e274f655dd4cb7134fa8dcfd
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-10-20 14:11:52 -0300

    Add missing CRUD services

--- frontend/src/services/WebSiteService.js
+++ frontend/src/services/WebSiteService.js
@@ -1,14 +1,46 @@
 import axios from 'axios'
 
+const service = axios.create({baseURL: 'http://localhost:3000'});
+
 const getWebSites = async () => {
   try {
-    const response = await axios.get('http://localhost:3000/sites')
-    return response.data;
+    const response = await service.get('/sites')
+    return response.data
   } catch (error) {
-    throw new error;
+    console.error(error)
+  }
+}
+
+const getWebSite = async (id) => {
+  try {
+    const response = await service.get(`/sites/${id}`)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const postWebSites = async (payload) => {
+  try {
+    const response = await service.post('/sites', payload)
+    return response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const deleteWebSite = async (id) => {
+  try {
+    const response = await service.delete(`/sites/${id}`)
+    return response.data
+  } catch (error) {
+    console.error(error)
   }
 }
 
 export default {
-  getWebSites
+  getWebSites,
+  getWebSite,
+  postWebSites,
+  deleteWebSite
 }
