96080d39ef36f0ef236dbd350567ac6cc54a7ca8
alexisarte <alexisrodriguezarteaga@gmail.com>
2023-10-27 17:53:30 -0300

    Create user profile component

---
+++ frontend/src/views/UserProfileView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { useAuthStore } from '@/stores/userAuthStore.js';
+const authStore = useAuthStore();
+let user = {
+  name: 'John Doe',
+  email: 'asdfa'
+}
+user = authStore.getUserData;
+
+const accesToken = authStore.getToken;
+</script>
+
+<template>
+  <div>
+    <h1>Profile Page</h1>
+    <div>
+      <div>
+        <div>
+          <!-- <img :src="user.picture" /> -->
+          <div>
+            <h2>{{ user.name }}</h2>
+            <span>{{ user.email }}</span>
+          </div>
+        </div>
+        <div>
+          <span title="User Token">{{ accessToken }}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
